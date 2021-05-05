9d9b5d626c1ed3a64413e096206ac0c4e01158b4
Bodeu97 <lukaszebo@gmail.com>
2021-05-05 18:53:13 +0200

    upd

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ import openpyxl
 
 # Zad1
 dtFrame = pd.read_excel('imiona.xlsx', header=0)
-print(dtFrame)
+# print(dtFrame)
 # Zad2
+# print(dtFrame.loc[dtFrame['Liczba'] < 1000])
+# print(dtFrame.loc[dtFrame['Imie']=='BOHDAN'])
+# print(dtFrame['Liczba'].sum())
+# Pomocniczy dataframe
+dt1=dtFrame.loc[(dtFrame['Rok']>2005) & (dtFrame['Rok']<2010)]
+# print(dt1['Liczba'].sum())
